f2d4948dc2f0135b9e48781005520e3112a41b60
Christopher Ponds <87038488+ShowMeYourLegos96@users.noreply.github.com>
2026-04-23 20:46:34 +0000

    created the function that would add data to a given column in a given dataframe. I also included ValueError just in case the column name is not found or if the user typed it in wrong.

---
+++ src/data_modification/data_modify.py
@@ -0,0 +1,29 @@
+#Making a tool that would allow the agent to add any data to any column
+@tool("add_data_to_column", return_direct=True)
+
+def add_data_to_column(df: pd.DataFrame, column_name: str, data) -> pd.DataFrame:
+    """
+    Add data to a specific column in the DataFrame.
+    Args:
+        df (pd:DataFrame): The input DataFrame.
+        column_name(str): The name of the column to which data should be added.
+        data: The data to be added to the column. This can be a single value or a list of values.
+        Returns:
+        pd.DataFrame: The updated DataFrame with the new data added to the specified column.
+    """
+    if column_name not in df.columns:
+        raise ValueError(f"Column '{column_name}' does not exist in the DataFrame.")
+    if isinstance(data, list):
+        if len(data) != len(df):
+            raise ValueError("Length of the data list must match the number of rows in the DataFrame.")
+        df[column_name] = data
+    else:
+        df[column_name] = data
+    return df
+
+
+    
+
+
+
+        
